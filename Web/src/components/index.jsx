7c459319f383e7d4a4545686c423dc61d4cfee33
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-10-05 23:17:22 -0300

    Finalizando Dashboard

--- Web/src/components/index.jsx
+++ Web/src/components/index.jsx
@@ -1,2 +1,3 @@
 export * from './Icon/index'
 export * from './input/index'
+export * from "./Card"
--- Web/src/pages/Dashboard/index.jsx
+++ Web/src/pages/Dashboard/index.jsx
@@ -1,8 +1,8 @@
-import {Icon} from '~/components'
+import {Icon,Card} from '~/components'
 
 export const Dashboard = ()=>(
     <div className="">
-        <header className="bg-red-500 text-white p-4">
+        <header className="bg-red-500 text-white ">
             <div className="container max-w-3xl p-4 flex justify-between">
                 <img src="../public/logo/logo-fundo-vermelho.svg" className="w-28 md:w-40"></img>
                 <a href='/profile'>
@@ -10,17 +10,32 @@ export const Dashboard = ()=>(
                 </a>
             </div>
         </header>
-        <main>
-            <section id='header' className=" bg-red-500 text-white p-4">
-                <div className='container max-w-3xl space-y-2'>
+        <main className=''>
+            <section id='header' className=" bg-red-500 text-white">
+                <div className='container max-w-3xl space-y-2 p-4'>
                 <span >Olá Eduardo</span>
                 <h3 className='text-2xl font-bold'>Qual o seu palpite?</h3>
 
                 </div>
             </section>
 
-            <section id='content'>
+            <section id='content' className='p-4 container max-w-3xl space-y-4'>
 
+                <Card 
+                timeA={{slug:"sui"}}
+                timeB={{slug:"cam"}}
+                match = {{time:"7:00"}}
+                />
+                <Card 
+                timeA={{slug:"uru"}}
+                timeB={{slug:"cor"}}
+                match = {{time:"7:00"}}
+                />
+                <Card 
+                timeA={{slug:"por"}}
+                timeB={{slug:"gan"}}
+                match = {{time:"7:00"}}
+                />
             </section>
         </main>
     </div>
