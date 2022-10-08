99affddb5cbc2c1827c26c40262f28ee6fe45799
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-10-08 20:05:29 -0300

    Criando BD

--- api/router.js
+++ api/router.js
@@ -1,7 +1,21 @@
 import Router from "koa-router";
+import { PrismaClient } from "@prisma/client";
 
 export const router = new Router();
+const prisma = new PrismaClient();
 
-router.get("/", async (ctx) => {
-  ctx.body = { ola: "mundo" };
+const users = [];
+
+router.get("/users", async (ctx) => {
+  ctx.body = users;
+});
+
+router.post("/users", async (ctx) => {
+  const user = {
+    username: ctx.request.body.username,
+  };
+
+  users.push(user);
+
+  ctx.body = user;
 });
