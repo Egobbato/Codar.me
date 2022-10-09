1e3ff464881c77319fc4a8441b94aa5eac05b97a
Eduardo Gobbato <102391940+Egobbato@users.noreply.github.com>
2022-10-08 22:32:36 -0300

    Iniciando as Integraçoes

--- api/router.js
+++ api/router.js
@@ -1,21 +1,8 @@
 import Router from "koa-router";
-import { PrismaClient } from "@prisma/client";
+import * as users from "./app/users/index.js";
 
 export const router = new Router();
-const prisma = new PrismaClient();
 
-const users = [];
+router.post("/users", users.create);
 
-router.get("/users", async (ctx) => {
-  ctx.body = users;
-});
-
-router.post("/users", async (ctx) => {
-  const user = {
-    username: ctx.request.body.username,
-  };
-
-  users.push(user);
-
-  ctx.body = user;
-});
+router.get("/users", users.list);
